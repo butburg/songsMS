8de675979734d13cb0e77ac3642f2c420fa4dc0a
butburg <fire.fm@hotmail.de>
2021-09-14 12:20:39 +0200

    set service for UserController
    Took 37 minutes

--- auth/src/main/java/authgrp/auth/controller/UserController.java
+++ auth/src/main/java/authgrp/auth/controller/UserController.java
@@ -2,8 +2,8 @@ package authgrp.auth.controller;
 
 import authgrp.auth.model.User;
 import authgrp.auth.repo.UserRepository;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import authgrp.auth.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,43 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class UserController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private final AuthService authService;
 
     public UserController(UserRepository repo) {
-        this.userRepository = repo;
+        this.authService = new AuthService(repo);
     }
 
     @GetMapping
     public ResponseEntity<Object> checkUser(@RequestHeader("Authorization") String authToken) {
-        try {
-            User user = userRepository.findByTokenLike(authToken);
-            if (user == null) throw new NullPointerException("No user with token" + authToken + "!");
-            return new ResponseEntity<Object>(user, HttpStatus.OK);
-        } catch (Exception ex) {
-            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
-        }
+        return authService.checkUser(authToken);
     }
 
     @PostMapping
     public ResponseEntity<Object> loginUser(@RequestBody User search) {
-
-        String searchedUserId = search.getUserId();
-        String searchedUserPw = search.getPassword();
-        User user = userRepository.findByUserId(searchedUserId);
-
-        if (user == null || user.getUserId() == null ||
-                user.getPassword() == null) {
-            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
-        }
-
-        if (user.getUserId().equals(searchedUserId) && user.getPassword().equals(searchedUserPw)) {
-            String token = user.generateAndSetToken();
-            HttpHeaders authHeader = new HttpHeaders();
-            authHeader.add("Authorization", token);
-            userRepository.save(user);
-            return new ResponseEntity<Object>(token, authHeader, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
-        }
+        return authService.loginUser(search);
     }
 }
---
+++ auth/src/main/java/authgrp/auth/service/AuthService.java
@@ -0,0 +1,48 @@
+package authgrp.auth.service;
+
+import authgrp.auth.model.User;
+import authgrp.auth.repo.UserRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author github.com/butburg (EW) on Sep 2021
+ */
+@Service
+public class AuthService {
+
+    private final UserRepository userRepository;
+
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public ResponseEntity<Object> checkUser(String authToken) {
+        User user = userRepository.findByTokenLike(authToken);
+        if (user == null) return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<Object>(user, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Object> loginUser(User searchedUser) {
+        String searchedUserId = searchedUser.getUserId();
+        String searchedUserPw = searchedUser.getPassword();
+        User user = userRepository.findByUserId(searchedUserId);
+
+        if (user == null || user.getUserId() == null ||
+                user.getPassword() == null) {
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
+
+        if (user.getUserId().equals(searchedUserId) && user.getPassword().equals(searchedUserPw)) {
+            String token = user.generateAndSetToken();
+            HttpHeaders authHeader = new HttpHeaders();
+            authHeader.add("Authorization", token);
+            userRepository.save(user);
+            return new ResponseEntity<Object>(token, authHeader, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
+    }
+}
