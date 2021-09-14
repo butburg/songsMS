a50907ad134adb8b641eb93f7252f07f14b0c9b1
butburg <fire.fm@hotmail.de>
2021-09-14 13:58:38 +0200

    add authorization to all methods for song
    Took 2 minutes

--- songs/src/main/java/songgrp/song/controller/SongController.java
+++ songs/src/main/java/songgrp/song/controller/SongController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import songgrp.song.Authorization;
 import songgrp.song.model.Song;
 import songgrp.song.repo.SongRepository;
 import songgrp.song.service.SongService;
@@ -15,7 +16,7 @@ import songgrp.song.service.SongService;
 
 @RestController
 @RequestMapping("/songs")
-public class SongController {
+public class SongController extends Authorization {
 
     @Autowired
     private final SongService songService;
@@ -24,19 +25,33 @@ public class SongController {
         this.songService = new SongService(songRepository);
     }
 
-
     // GET one song /songs/1
     // Ausgabeformat JSON und XML
     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Object> getSong(@PathVariable(value = "id") Integer id) {
-        return songService.getSong(id);
+    public ResponseEntity<Object>
+    getSong(
+            @RequestHeader("Authorization") String authToken,
+            @PathVariable(value = "id") Integer id) {
+        try {
+            return songService.getSong(authorizeUser(authToken), id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
     // GET all songs /songs
     // Ausgabeformat JSON und XML
     @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Object> getAllSongs() {
-        return songService.getAllSong();
+    public ResponseEntity<Object>
+    getAllSongs(
+            @RequestHeader("Authorization") String authToken) {
+        try {
+            return songService.getAllSong(authorizeUser(authToken));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
     // POST new song /songs
@@ -44,8 +59,16 @@ public class SongController {
     // „title“-Attribute darf nicht leer sein, wenn erfolgreich, legt neuen Song an,
     // schickt Statuscode 201 und URI (/songs/) zur neuen Ressource im „Location“-Header zurück
     @PostMapping(consumes = "application/json", produces = "application/json")
-    public ResponseEntity<Object> addSong(@RequestBody Song song) {
-        return songService.addSong(song);
+    public ResponseEntity<Object>
+    addSong(
+            @RequestHeader("Authorization") String authToken,
+            @RequestBody Song songToAdd) {
+        try {
+            return songService.addSong(authorizeUser(authToken), songToAdd);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
     // PUT update song /songs/1
@@ -53,19 +76,37 @@ public class SongController {
     // Update nur, wenn „songId“ in URL gleich „id“ in Payload „title“-Attribute darf nicht leer sein.
     // Wenn Update erfolgreich, dann nur Statuscode 204 zurückschicken, ansonsten 400 bzw. 404
     @PutMapping(value = "/{id}")
-    public ResponseEntity<Object> updateSong(@RequestBody Song songToPut, @PathVariable(value = "id") Integer id) {
-        return songService.updateSong(id, songToPut);
+    public ResponseEntity<Object>
+    updateSong(
+            @RequestHeader("Authorization") String authToken,
+            @PathVariable(value = "id") Integer id,
+            @RequestBody Song songToPut) {
+        try {
+            return songService.updateSong(authorizeUser(authToken), id, songToPut);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
     // DELETE a song by id
     // wenn Song vorhanden und Löschen erfolgreich, dann nur Statuscode 204 zurückschicken, ansonsten 400 bzw. 404
     @DeleteMapping(value = "/{id}")
-    public ResponseEntity<Object> deleteSong(@PathVariable(value = "id") Integer id) {
-        return songService.deleteSong(id);
+    public ResponseEntity<Object>
+    deleteSong(
+            @RequestHeader("Authorization") String authToken,
+            @PathVariable(value = "id") Integer id) {
+        try {
+            return songService.deleteSong(authorizeUser(authToken), id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<Object>(HttpStatus.UNAUTHORIZED);
+        }
     }
 
     @DeleteMapping
-    public ResponseEntity<Object> deleteWrongPath() {
+    public ResponseEntity<Object>
+    deleteWrongPath() {
         return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
     }
 }
